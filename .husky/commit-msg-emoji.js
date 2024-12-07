b5e19402afb94c17ab5a6a1f1a3db4b5309ae914
huangjie <huangjie6@codemao.cn>
2024-12-07 14:59:38 +0800

    ✨ feat: 初始化项目配置

---
+++ .husky/commit-msg-emoji.js
@@ -0,0 +1,32 @@
+import { readFileSync, writeFileSync } from 'fs';
+
+const typeToEmojiMap = {
+  chore: '🎨',
+  feat: '✨',
+  fix: '🐛',
+};
+
+// 数组at方法：从前往后索引0到length-1，从右往左-1到-length,且不改变原数组
+// eslint-disable-next-line no-undef
+const COMMIT_EDITING_FILEPATH = process.argv.at(-1);
+
+const inputMsg = readFileSync(COMMIT_EDITING_FILEPATH, 'utf8');
+
+writeFileSync(
+  COMMIT_EDITING_FILEPATH,
+  transform(inputMsg, typeToEmojiMap),
+  'utf8',
+);
+
+function transform(inputMsg, typeToEmojiMap) {
+  const [type, emoji] =
+    Object.entries(typeToEmojiMap).find(([type]) =>
+      inputMsg.startsWith(type),
+    ) ?? [];
+
+  if (type) {
+    return inputMsg.replace(new RegExp(`^${type}`), `${emoji} $&`);
+  }
+
+  throw new Error('Invalid type');
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+### 初始化配置
+
+1. vue3
+2. vite
+3. ts
+4. eslint
+5. preettier
+6. styleliint
+7. husky
+8. pnpm
